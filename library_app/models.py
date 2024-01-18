92f8a35c79a885bb52ba597e78a4eda258e4eb16
festus <festus.hoopa@gmail.com>
2024-01-18 15:23:02 +0530

    first commit

---
+++ library_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+  Book_Name = models.CharField(max_length=50)
+  Author = models.CharField(max_length=50)
+  Price = models.IntegerField()
+  Description = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.Book_Name
+  
